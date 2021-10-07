04cccd1dfbb45ce2de95d80cd114a61e59d10318
Andres Garcia Fernandez <andres.garcia.fernandez@hp.com>
2021-10-07 16:30:05 -0600

    Template Update

--- Python_Analysis/Library/get_data.py
+++ Python_Analysis/Library/get_data.py
@@ -3,18 +3,28 @@ import os
 import codecs
 import pyodbc
 import json
+import argparse
+import re
+import win32com.client
 import datetime as dt
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine
+import xlwings as xw
 import tkinter as tk
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+import fiscalyear as fy
 from tkinter import messagebox
+from sqlalchemy import create_engine
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
-import xlwings as xw
-import win32com.client
 from win32com.client import Dispatch, constants
+
+# from __future__ import print_function
+from pptx import Presentation
+from pptx.util import Inches
 
 
 class Get_Data:
@@ -33,7 +43,13 @@ class Get_Data:
         self.__output_path = None
         self.__query_path = None
         self.__input_path = None
-        self.__today = dt.datetime.today()
+        self.__today = dt.datetime.today() - pd.DateOffset(months=1)
+        self.__monthname_long = self.__today.strftime("%B")
+        self.__monthname_short = self.__today.strftime("%b")
+        self.__monthyear = self.__today.strftime("%m%y")
+        self.__fiscal_year = None
+        self.__current_quarter = None
+        self.__fiscal_quarter = None
 
     # setter functions
     """
@@ -49,6 +65,18 @@ class Get_Data:
     def set_input_path(self, path):
         self.__input_path = path
 
+    def set_fiscal_year(self, s_month, s_day, f_year, s_year="previous"):
+        self.__fiscal_year = fy.FiscalYear(f_year)
+        fy.setup_fiscal_calendar(
+            start_year=s_year, start_month=s_month, start_day=s_day
+        )
+        self.__fiscal_quarter = str(
+            fy.FiscalQuarter.current().prev_fiscal_quarter
+        ).split(" ")[0]
+        self.__current_quarter = str(
+            fy.FiscalQuarter.current().prev_fiscal_quarter
+        ).split(" ")[1]
+
     # set engine db
     """
         Function to initialize the engine to query the specific data base provider
@@ -56,7 +84,7 @@ class Get_Data:
     """
 
     def set_engine(self):
-        credentials = "C:/Users/garciand/OneDrive - HP Inc/Desktop/Python_Analytics/NoteBooks/Credentials/{0}.json".format(
+        credentials = "C:/Users/garciand/OneDrive - HP Inc/Desktop/Python_Analytics/Python_Analysis/Credentials/{0}.json".format(
             self.__credentials
         )
         with open(credentials) as get:
@@ -276,3 +304,93 @@ class Get_Data:
             file_name,
             protect_file,
         )
+
+    def ppt_analyzer(input, output):
+
+        """Take the input file and analyze the structure.
+        The output file contains marked up information to make it easier
+        for generating future powerpoint templates.
+        """
+        prs = Presentation(input)
+        # Each powerpoint file has multiple layouts
+        # Loop through them all and  see where the various elements are
+        for index, _ in enumerate(prs.slide_layouts):
+            slide = prs.slides.add_slide(prs.slide_layouts[index])
+            # Not every slide has to have a title
+            try:
+                title = slide.shapes.title
+                title.text = "Title for Layout {}".format(index)
+            except AttributeError:
+                print("No Title for Layout {}".format(index))
+            # Go through all the placeholders and identify them by index and type
+            for shape in slide.placeholders:
+                if shape.is_placeholder:
+                    phf = shape.placeholder_format
+                    # Do not overwrite the title which is just a special placeholder
+                    try:
+                        if "Title" not in shape.text:
+                            shape.text = "Placeholder index:{} type:{}".format(
+                                phf.idx, shape.name
+                            )
+                    except AttributeError:
+                        print("{} has no text attribute".format(phf.type))
+                    print("{} {}".format(phf.idx, shape.name))
+        prs.save("./Templates/" + output)
+            
+    def ppt_export(self, file_type, org=None):
+
+         # determine output file HPI Total or L1 Org Dashboard
+        if file_type == "HPI":
+            output_file = "HPI DEI Dashboard {0} {1}".format(
+                self.__current_quarter, self.__fiscal_quarter
+            )
+        elif file_type == "L1 ORG":
+            output_file = "DEI {0} Dashboard {1}".format(org, self.__monthyear)
+
+        # set slides to be updated
+        prs = Presentation("./Templates/HP_Presentation_Template.pptx")
+        slide_1 = prs.slides[0]
+        slide_4 = prs.slides[3]
+        slide_6 = prs.slides[5]
+        slide_7 = prs.slides[6]
+        slide_8 = prs.slides[7]
+        slide_9 = prs.slides[8]
+
+        # set placeholders to be updated
+        prs_sub_1 = slide_1.placeholders[1]
+        prs_sub_4 = slide_4.placeholders[0]
+        prs_sub_6 = slide_6.placeholders[0]
+        prs_sub_7 = slide_7.placeholders[0]
+        prs_sub_8 = slide_8.placeholders[0]
+        prs_sub_9 = slide_9.placeholders[0]
+
+        # update placeholders
+        if file_type == "HPI":
+            prs_sub_1.text = "As of {0} month, end/{1}".format(
+                self.__monthname_long, self.__current_quarter
+            )
+        elif file_type == "L1 ORG":
+            prs_sub_1.text = "{0} (As of {1} month, end/{2})".format(
+                org, self.__monthname_long, self.__current_quarter
+            )
+
+        prs_sub_4.text = (
+            self.__fiscal_quarter + " " + self.__current_quarter + " Headcount"
+        )
+        prs_sub_6.text = "{0}/{1} Status to Diversity Targets (Company Level)".format(
+            self.__monthname_short, self.__current_quarter
+        )
+        prs_sub_7.text = "{0}/{1} Active Headcount by Organization / MRU".format(
+            self.__monthname_short, self.__current_quarter
+        )
+        prs_sub_8.text = (
+            "{0}/{1} Active Headcount by Organization / MRU (Absolute values)".format(
+                self.__monthname_short, self.__current_quarter
+            )
+        )
+        prs_sub_9.text = "{0}/{1} US Ethnic Groups Not Self-Identified HC by Organization / MRU".format(
+            self.__monthname_short, self.__current_quarter
+        )
+
+        # save updated template
+        prs.save(self.__output_path + file_type + "\\" + output_file + ".pptx")
